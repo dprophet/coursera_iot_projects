354922421211def57993dbf8feca27a83ea357cc
Erik Anderson <eanders@pobox.com>
2017-01-07 21:35:30 -0500

    Change to utc time.

--- camera/arducam/openwrt/SoftlayerUpload.py
+++ camera/arducam/openwrt/SoftlayerUpload.py
@@ -76,7 +76,7 @@ class EmbedSensorDataInJPEG:
                 my_mac = ':'.join(("%012X" % mac)[i:i + 2] for i in range(0, 12, 2))
                 oDate = datetime.datetime.utcnow()
                 sTime = oDate.strftime("%Y:%m:%d %H:%M:%S")
-                oDict['time_taken'] = oDate.isoformat()
+                oDict['utc_time'] = oDate.isoformat()
                 oDict['mac_address'] = my_mac
                 oDict['image_url'] = self._sFullURL
                 sNewData = json.dumps(oDict)
