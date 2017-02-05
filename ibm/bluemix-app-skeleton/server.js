9378823410432caf49fbbc9c6fc85f16fc1f4b2a
Erik Anderson <eanders@pobox.com>
2017-02-05 13:11:25 -0500

    Final assignment.

--- ibm/bluemix-app-skeleton/server.js
+++ ibm/bluemix-app-skeleton/server.js
@@ -13,6 +13,19 @@ var cfenv = require('cfenv');
 var cfappEnv = cfenv.getAppEnv();
 var app = express();
 
+if ( process.env.VCAP_SERVICES) {
+  var env = JSON.parse(process.env.VCAP_SERVICES);
+  appClientConfig = {
+         'org' : env["iotf-service"][0].credentials.org,
+         'id' : 'erik-nodeserver',
+         'auth-key' : env["iotf-service"][0].credentials.apiKey,
+         'auth-token' : env["iotf-service"][0].credentials.apiToken
+       }
+}
+
+
+var appClient = new iotf.IotfApplication(appClientConfig);
+
 app.get('/status', function(req, res) {
   res.status(200).json({ message: 'iot service running' });
 });
@@ -21,4 +34,5 @@ module.exports = app.listen(cfappEnv.port, function() {
   console.log('server started on ' + cfappEnv.url);
   var service = new Service(appClient);
   // TODO use service to connect here
+  service.connect();
 });
